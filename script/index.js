e8d0392de35fa0f2577811ec994a03b2a5bb42a9
Ahmad <totsukamail3278@gmail.com>
2022-06-17 12:53:44 +0700

    modified some files

--- script/index.js
+++ script/index.js
@@ -1,1 +1,14 @@
-/* Tidak ada kode disini :v */
+let person = {
+    nama: 'Achmad',
+    umur: 16,
+    hobi: ['Coding', ' Playing piano', ' Playing Games']
+}
+
+const to_be = person.hobi.length > 1 ? "hobby are" : "hobbies is";
+const sayHello = () => `Hi, I'm ${person.nama}, I'm ${person.umur} years old. My ${to_be} ${person.hobi}`;
+
+function callBack(name){
+    console.log(name());
+}
+
+callBack(sayHello);
