28c986cea2e1ce97baf86cd6ecf5891ee88beeaa
Ahmad <totsukamail3278@gmail.com>
2022-06-17 16:33:13 +0700

    modified index.js

--- script/index.js
+++ script/index.js
@@ -3,12 +3,7 @@ let person = {
     umur: 16,
     hobi: ['Coding', ' Playing piano', ' Playing Games']
 }
-
 const to_be = person.hobi.length > 1 ? "hobby are" : "hobbies is";
 const sayHello = () => `Hi, I'm ${person.nama}, I'm ${person.umur} years old. My ${to_be} ${person.hobi}`;
-
-function callBack(name){
-    console.log(name());
-}
-
+const callBack = name => console.log(name());
 callBack(sayHello);
