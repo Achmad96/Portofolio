a334a05255f3a6a11f24cb83af90ee55afe31a2c
Ahmad <totsukamail3278@gmail.com>
2022-06-18 08:30:54 +0700

    deleted index.js

--- script/index.js
+++
@@ -1,9 +0,0 @@
-let person = {
-    nama: 'Achmad',
-    umur: 16,
-    hobi: ['Coding', ' Playing piano', ' Playing Games']
-}
-const to_be = person.hobi.length > 1 ? "hobby are" : "hobbies is";
-const sayHello = () => `Hi, I'm ${person.nama}, I'm ${person.umur} years old. My ${to_be} ${person.hobi}`;
-const callBack = name => console.log(name());
-callBack(sayHello);
